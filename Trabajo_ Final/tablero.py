f9259508408d0db25afbe79a881f6aad173f034d
MetaboClaud <99832472+MetaboClaud@users.noreply.github.com>
2022-05-15 21:26:26 +0200

    Correccion metodos y funciones clase tablero
    Se importa la clase bote para  poder usarla dentro de las propiedades tablero.
    Se crean métodos para imprimir el tablero y el juego
    Se crea el metodo de final de partida
    
    Se corrige la funcion "dado" para que al acceder a la longitud de las coordenadas sea del barco y no del objeto tablero. Ademas se añaden breaks para no iterar mas veces de las necesarias

--- Trabajo_ Final/tablero.py
+++ Trabajo_ Final/tablero.py
@@ -1,63 +1,77 @@
 import numpy as np
-import random
+import bote as b
 
 class Tablero:
-    
-                                                  
-                                                                            ### Idea magica, para comprobar si se ha disparado, cuando se dispare hacer esto
-                                                                            ###         if coordenadas in tablero.copia == "": dispara ; si no vuelve a elegir coordenadas
-    esloras=[4,3,3,2,2,2,1,1,1,1]                                           ### Es el numero de barcos con cada eslora : 1 de 4, 2 de 3, 3 de 2 , 4 de 1
-    
+                                                                  
+    esloras=[4,3,3,2,2,2,1,1,1,1]         ### Es el numero de barcos con cada eslora : 1 de 4, 2 de 3, 3 de 2 , 4 de 1
+
     
     def __init__(self,nombre):
-        self.cuerpo=np.full((10,10)," ")                                                ###Este sera el tablero donde iran nuestros barcos
-        self.copia=np.full((10,10)," ")                                                 ###Este sera el tablero donde iran nuestros disparos al enemigo
-        self.nombre=nombre
-        self.barcos=[]
+        self.tablero_propio= np.full((10,10)," ")               ###Este sera el tablero donde iran nuestros barcos
+        self.tablero_disparo= np.full((10,10)," ")              ###Este sera el tablero donde iran nuestros disparos al enemigo
+        self.nombre= nombre
+        self.barcos= []
         for i in self.esloras:
             self.annadirbarco(i)
     
+
+
     def annadirbarco(self,eslora):
-        bote=Bote(eslora,self.cuerpo)
-        self.barcos.append(bote)
+        barco= b.Bote(eslora,self.tablero_propio)
+        self.barcos.append(barco)
         
 
+    def imprimir_tablero(self, tablero):
+        titulo = np.arange(0,10)
+        tablero_titulos = np.insert(tablero, 0, titulo, axis= 0) 
+        columna_titulo = np.append(["|"], titulo )
+        tablero_titulos = np.insert(tablero_titulos, 0, columna_titulo, axis= 1) 
+        print(tablero_titulos)
+
+    def imprimir_tableros_juego(self):
+        print("___________________________________________________________")
+        print(f"Nombre del jugador :  {self.nombre}")
+        print("Tablero propio")
+        self.imprimir_tablero(self.tablero_propio)
+        print("Tablero de disparos")
+        self.imprimir_tablero(self.tablero_disparo)
+    
+
+
     def dado(self,coordenadas:tuple):
-        if self.cuerpo[int(coordenadas[0]),int(coordenadas[1])]=="O":
-            self.cuerpo[int(coordenadas[0]),int(coordenadas[1])]="X"
+        if self.tablero_propio[int(coordenadas[0]),int(coordenadas[1])]=="O":
+            self.tablero_propio[int(coordenadas[0]),int(coordenadas[1])]="X"
             
-
-
             for i in self.barcos:
                 if coordenadas in i.coordenadas:
-                    if i.veces+1==len(self.coordenadas):
-                        i.estado="hundido"                                  ###Pondre esto para a la hora de imprimir el mensaje por pantalla hacer print("Barco",estado)
-                        """
-                        Poner sonidito barco hundido
-                        """
+                    if i.veces+1 == len(i.coordenadas):
+                        i.estado="hundido"
+                        print("Hundido")                                  ###Pondre esto para a la hora de imprimir el mensaje por pantalla hacer print("Barco",estado)
+                        break            #para que no este iterando en todos los barcos una vez localizado el hundido
                     else:
                         i.veces+=1
                         i.estado="tocado"
                         print("Tocado")                                     ###Pondre esto para a la hora de imprimir el mensaje por pantalla hacer print("Barco",estado)
-                        """ 
-                        Poner sonidito barco tocado
-                        """
-
+                        break
             if self.comprobar_fin():
-                self.terminar_partida()
+                return 1000                                 #Este valor indicaría que ha terminado la partida _cbba
             else:
                 return 1
         else:
-            self.cuerpo[int(coordenadas[0]),int(coordenadas[1])]="@"                                     ###Poner @ como agua en vez de -(?)
+            self.tablero_propio[int(coordenadas[0]),int(coordenadas[1])]="@"                                     ###Poner @ como agua en vez de -(?)
             print("Agua")
             return 0
 
-    def comprobar_fin(self):
-        if "X" in self.cuerpo:
+
+    def comprobar_fin(self):                                            ##Comprobar si queda alguna coordenada sin golpear
+        if "O" in self.tablero_propio:
             return False
         else:
             return True
     
-    def terminar_partida():
-        pass                                                                ### annadir pantalla final y menu sobre que hacer ahora
-        
+    def terminar_partida(self,jugador_oponente):
+        print(f"Enhorabuena has ganado la partida {self.nombre} ")
+        self.imprimir_tableros_juego()
+        print("Se imprimen también los tableros del oponente")
+        jugador_oponente.imprimir_tableros_juego()
+
